c8a7104d176516efc7e3c14e22ba6fff6ce8c700
OM-Coder95 <ommore8855@gmail.com>
2026-09-25 18:56:47 +0530

    remove funcionality Completed

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+
+git and gitHub
+
+what is git?
+-> Git is a version control system used to track changes in your code and manage different versions of a project...
+
+Working Directory => git => gitHub
+
+Flow Project gitHub kaise push karenge?
+
+Project => Staging Area => commit => gitHub 
+
+WHAT IS STAGING AREA?
+
+Staging area ek temporary area hai jaha tum git ko batae ho ki "In changes ko next commit mein save karna hai..."
+
+what is commit?
+
+kiye gaye staged changes ko git repo mein save karo...
+
+
+Commands
+
+1. git init = to initialize git repo
+2. git add . = add files to staging area
+3. git commit -m "message" = commit the staged changes/files
+
+
+Branch Related Commands
+
+1. check = git branch
+2. create = git branch -c branchName
+3. switch = git switch branchName
+4. delete = git branch -d branchName
+5. modify = git branch -M newBranchName
+
+function removeStudent(ele) {
+    // remove code
+
+
+
+    
+}
